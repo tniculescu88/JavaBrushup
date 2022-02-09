82de11f3db5a3e4557f3ec11fdacd0ff920d91b3
tudorN <tudor.niculescu88@gmail.com>
2022-02-09 09:51:22 +0200

    FirstRevision

---
+++ src/CoreJavaBrushup1.java
@@ -0,0 +1,33 @@
+
+public class CoreJavaBrushup1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5;
+		String webSite = "Rahul Shetty Academy";
+		char letter = 'r';
+		boolean myCard = true;
+		
+		System.out.println(num + "is the val is the var");
+		
+		//Arrays - 
+		int[] arr = new int[5];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		arr[4] = 5;
+		
+		int[] arr2 = {1,2,3,4,5,6};
+		
+	
+	
+		String[] name = {"rahul", "shetty", "selenium"};
+		
+		for (String s:name)
+		{
+			System.out.println(s);
+		}
+
+	}
+}
---
+++ src/JavaBrushup2.java
@@ -0,0 +1,21 @@
+
+public class JavaBrushup2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr2 = {1,2,4,5,6,7,8,9,10,122};
+		
+		for(int i = 0; i<arr2.length; i++)
+		{
+			if (arr2[i] % 2 == 0)
+			{
+				System.out.println(arr2[i]);
+			}
+		}
+		
+		
+
+	}
+
+}
